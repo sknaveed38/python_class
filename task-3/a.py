695888d76812f2e10ff686c996bf5129cbab6877
SHAIK NAVEED <sknaveedsk20@gmail.com>
2025-09-02 06:34:16 +0000

    upload

---
+++ task-3/a.py
@@ -0,0 +1,8 @@
+
+import mymath # pyright: ignore[reportMissingImports]
+a = 10
+b = 5
+print("Addition:", mymath.add(a, b))
+print("Subtraction:", mymath.subtract(a, b))
+print("Multiplication:", mymath.multiply(a, b))
+print("Division:", mymath.divide(a, b))
---
+++ task-3/b.py
@@ -0,0 +1,11 @@
+import mathfunction
+import areafunction 
+# Using math functions
+print("Addition:", mathfunction.add(10, 5)) 
+print("Subtraction:", mathfunction.subtract(10, 5))
+print("Multiplication:", mathfunction.multiply(10, 5))
+print("Division:", mathfunction.divide(10, 5))
+# Using area functions
+print("Circle Area (radius=7):", areafunction.circle_area(7))
+print("Rectangle Area (5x10):", areafunction.rectangle_area(5, 10))
+print("Triangle Area (base=6, height=8):", areafunction.triangle_area(6, 8))
